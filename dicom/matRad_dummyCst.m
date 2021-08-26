d0a2cc50eed3291f88ad9707887cf1326d6b00b7
cmodha <74353527+cmodha@users.noreply.github.com>
2021-08-26 16:17:35 +0100

    Add files via upload

---
+++ dicom/matRad_dummyCst.m
@@ -0,0 +1,74 @@
+function cst = matRad_dummyCst(ct)
+% matRad function to create a dummy cst struct for a ct
+% 
+% call
+%   cst = matRad_dummyCst(ct)
+%
+% input
+%   ct: matRad ct struct
+%
+% output
+%   cst:            matRad cst struct
+%
+% References
+%   -
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Copyright 2015 the matRad development team. 
+% 
+% This file is part of the matRad project. It is subject to the license 
+% terms in the LICENSE file found in the top-level directory of this 
+% distribution and at https://github.com/e0404/matRad/LICENSES.txt. No part 
+% of the matRad project, including this file, may be copied, modified, 
+% propagated, or distributed except according to the terms contained in the 
+% LICENSE file.
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+warning('Did not find RTSS. Creating dummy segmentation for matRad.');
+
+% allocate
+% cst = cell(1,6);
+% function val = createSphere(ct, center_x,center_y,center_z,radius)
+%     nx= 0;ny=0;nz = 0;
+%     indeces = [];
+%     col_x = size(ct.x);
+%     col_y = size(ct.y);
+%     for x=ct.x
+%         for y=ct.y
+%             for z=ct.z
+%                 if (x-center_x)^2+(y-center_y)^2 + (z-center_z)^2 < radius^2
+%                     index = col_x*col_y*nz+col_x*nx+ny;
+%                     indeces.append(index);
+%                 end
+%                 nz= nz+1;
+%             end
+%             ny=ny+1;
+%         end
+%         nx=nx+1;
+%     end
+%     val = indeces;
+% end
+  
+
+% fill
+cst{1,1}          = 0; % first organ has number 0
+cst{1,2}          = 'dummyContour';
+cst{1,3}          = 'TARGET';
+cst{1,4}{1}       = find(ct.cubeHU{1}>0.1);  
+% cst{1,4}{1} = [];
+% cst{1,4}{1}       = createSphere(ct, 200,150,124,50);
+
+cst{1,5}.Priority = 1;       
+
+% set default parameter for biological planning
+cst{1,5}.alphaX   = 0.1;
+cst{1,5}.betaX    = 0.05;
+cst{1,5}.Visible  = 1;
+cst{1,5}.visibleColor = [0 0 0];
+
+% define no objcetives   
+cst{1,6}          = [];
+
+end
